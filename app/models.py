fd822a345f72551d5c6c878d65433385e3be2e73
David Vidal Ramírez <david650991@gmail.com>
2026-01-20 08:21:30 -0600

    Feature: Implementación completa de 2FA (TOTP) con bloqueo de login

--- app/models.py
+++ app/models.py
@@ -3,14 +3,14 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-    # Esta función permite a Flask-Login recargar al usuario usando su ID de sesión
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    # Definimos las columnas de nuestra tabla 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    # NUEVA COLUMNA: Aquí guardaremos el secreto del Authenticator
+    otp_secret = db.Column(db.String(32), nullable=True) 
 
     def __repr__(self):
         return f"User('{self.username}')"
